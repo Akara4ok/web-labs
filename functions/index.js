e00380202582874068ef1cddd7541eda36671fb3
Akara4ok <vladshpilka86@gmail.com>
2021-10-22 16:21:36 +0300

    change cors

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,10 @@ app.use(
 );
 
 app.use((req, res, next) => {
-    res.set('Access-Control-Allow-Origin', '*');
+    res.set(
+        'Access-Control-Allow-Origin',
+        'https://mail-sender-10d73--pr6-lab2-f0t2q7r2.web.app',
+    );
     res.set('Access-Control-Allow-Headers', '*');
     res.set('Access-Control-Allow-Methods', '*');
     next();
