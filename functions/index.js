079598457c3c5bdb5b92f3ae0b2e966b5a7fdc42
Akara4ok <vladshpilka86@gmail.com>
2021-10-28 17:56:08 +0300

    add rate limit

--- functions/index.js
+++ functions/index.js
@@ -19,57 +19,91 @@ function validateEmail(email) {
     return re.test(String(email).toLowerCase());
 }
 
+const rateLimit = {
+    ipNumberCalls: 5,
+    timeSeconds: 30,
+    ipData: new Map(),
+};
+
 exports.api = functions.https.onRequest(async (req, res) => {
     const headers = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+        'Access-Control-Max-Age': 2592000,
         'Access-Control-Allow-Headers': '*',
     };
 
     if (req.method === 'OPTIONS') {
-        res.writeHead(204, headers);
+        res.writeHead(200, headers);
         res.end();
-        //return;
-    } else {
-        let isSuccess = true;
-        let isNameCorrect = true;
-        let isEmailCorrect = true;
-        let isMessageCorrect = true;
-        const cleanMessage = sanitizeHtml(req.body.message);
-        if (req.body.name === '') isNameCorrect = false;
-        if (!validateEmail(req.body.email)) isEmailCorrect = false;
-        if (cleanMessage === '') isMessageCorrect = false;
-        if (isNameCorrect && isEmailCorrect && isMessageCorrect) {
-            const output = `<p>${cleanMessage}</p>`;
-            const transporter = nodemailer.createTransport({
-                host: functions.config().email.host,
-                port: 587,
-                secure: false,
-                auth: {
-                    user: functions.config().email.address,
-                    pass: functions.config().email.pass,
-                },
-                tls: {
-                    rejectUnauthorized: false,
-                },
-            });
-
-            await transporter.sendMail({
-                from: `${req.body.name} <${functions.config().email.address}>`,
-                to: `${req.body.email}`,
-                subject: 'Hello',
-                text: 'Hello world?',
-                html: output,
-            });
-        } else {
-            isSuccess = false;
-        }
-
-        res.json({
-            isNameCorrect,
-            isEmailCorrect,
-            isMessageCorrect,
-            isSuccess,
-        });
+        return;
     }
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    let isSuccess = true;
+    let isNameCorrect = true;
+    let isEmailCorrect = true;
+    let isMessageCorrect = true;
+
+    const currentIp = req.headers['fastly-client-ip'];
+    let currentIpUser = {};
+    const currentTime = new Date();
+    if (rateLimit.ipData.get(currentIp) === undefined) {
+        rateLimit.ipData.set(currentIp, { count: 1, time: currentTime });
+    } else {
+        currentIpUser = rateLimit.ipData.get(currentIp);
+        if (
+            currentIpUser.count + 1 > rateLimit.ipNumberCalls ||
+            currentTime - currentIpUser.time <= rateLimit.timeSeconds * 1000
+        ) {
+            res.status(429).json({
+                isNameCorrect,
+                isEmailCorrect,
+                isMessageCorrect,
+                isSuccess: false,
+            });
+            return;
+        }
+    }
+    currentIpUser = rateLimit.ipData.get(currentIp);
+    currentIpUser.count += 1;
+    currentIpUser.time = new Date();
+    rateLimit.ipData.set(currentIp, currentIpUser);
+
+    const cleanMessage = sanitizeHtml(req.body.message);
+    if (req.body.name === '') isNameCorrect = false;
+    if (!validateEmail(req.body.email)) isEmailCorrect = false;
+    if (cleanMessage === '') isMessageCorrect = false;
+    if (isNameCorrect && isEmailCorrect && isMessageCorrect) {
+        const output = `<p>${cleanMessage}</p>`;
+        const transporter = nodemailer.createTransport({
+            host: functions.config().email.host,
+            port: 587,
+            secure: false,
+            auth: {
+                user: functions.config().email.address,
+                pass: functions.config().email.pass,
+            },
+            tls: {
+                rejectUnauthorized: false,
+            },
+        });
+
+        await transporter.sendMail({
+            from: `${req.body.name} <${functions.config().email.address}>`,
+            to: `${req.body.email}`,
+            subject: 'Hello',
+            text: 'Hello world?',
+            html: output,
+        });
+    } else {
+        isSuccess = false;
+    }
+
+    res.json({
+        isNameCorrect,
+        isEmailCorrect,
+        isMessageCorrect,
+        isSuccess,
+    });
 });
