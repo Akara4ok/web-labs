f4486e9e4e0c1a20ee06849c0cda0ca3d6b83b8b
Akara4ok <vladshpilka86@gmail.com>
2021-11-25 13:56:36 +0200

    try to add env

--- Lab3/Frontend/src/HasuraAPI/config.js
+++ Lab3/Frontend/src/HasuraAPI/config.js
@@ -1,5 +1,4 @@
 export const config = {
-    link: 'subtle-dove-32.hasura.app/v1/graphql',
-    password:
-        'SYfy99PqMy0EFUZ2evxFbwe4Q77eIeTdP5tibLRz2M0R1ZMjc8zrc0BEoUl1nm4M',
+    link: process.env.REACT_APP_HASURA_LINK,
+    password: process.env.REACT_APP_HASURA_PASSWORD,
 };
