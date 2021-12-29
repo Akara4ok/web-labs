d3a37fb9ad379759edcdf446ee619b6ee2811b43
Akara4ok <vladshpilka86@gmail.com>
2021-12-29 19:16:49 +0200

    add env vars

--- Lab3/Frontend/src/Components/Layout/Layout.jsx
+++ Lab3/Frontend/src/Components/Layout/Layout.jsx
@@ -28,12 +28,12 @@ class Layout extends React.PureComponent {
     componentDidMount() {
         this.setState({ provider: new firebase.auth.GoogleAuthProvider() });
         firebase.initializeApp({
-            apiKey: 'AIzaSyABJPnlkCs5CTvOojbtJb1CJDdqKgnE6GY',
-            authDomain: 'notes-ecba8.firebaseapp.com',
-            databaseURL: 'https://notes-ecba8-default-rtdb.firebaseio.com',
-            projectId: 'notes-ecba8',
-            storageBucket: 'notes-ecba8.appspot.com',
-            messagingSenderId: '423215069575',
+            apiKey: process.env.REACT_APP_API_KEY,
+            authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+            databaseURL: process.env.REACT_APP_DATABASE_URL,
+            projectId: process.env.REACT_APP_PROJECT_ID,
+            storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+            messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
         });
         firebase.auth().onAuthStateChanged(async user => {
             if (user) {
